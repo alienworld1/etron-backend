0a5742407cf0749a8526f0b74c70a6971b342209
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2025-02-17 15:43:59 +0000

    Basic setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const createError = require('http-errors');
+const express = require('express');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', indexRouter);
+
+app.use((req, res, next) => createError(404));
+
+app.use((err, req, res, next) => {
+  const error = req.app.get('env') === 'development' ? err : {};
+  const status = err.status || 500;
+  res.status(status).json({
+    status,
+    message: err.message,
+    error,
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
